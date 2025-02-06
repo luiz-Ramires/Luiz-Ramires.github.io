07ef3448417d64741023e6ba297460e797d57138
Luiz-Ramires <luizramires@gmail.com>
2025-02-05 22:33:49 -0400

    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
   .nav {
     display: flex;
     justify-content: center;
-    background: #7a26aa;
+    background: #3d3d3d;
     padding: 10px;
   }
   .nav a {
@@ -49,7 +49,7 @@
     gap: 10px;
     overflow-x: auto;
     padding: 10px;
-    background-color: #7d138b;
+    background-color: #353535;
   }
   .movie-card {
     flex: 0 0 auto;
